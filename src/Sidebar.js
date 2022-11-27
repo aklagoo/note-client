279847fc3dfff00c803a3eec7b403c7f1a197bd3
Archan Lagoo <aklagoo@gmail.com>
2022-11-26 23:07:49 -0700

    Added Sidebar dummy note

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,25 @@
 import './Sidebar.css';
 
+function Note() {
+    return (
+        <div className="Note">
+            <h3>Title</h3>
+            <p>
+                From which we spring quasar rings of Uranus a still more glorious dawn awaits
+                laws of physics the sky calls to us. Hearts of the stars venture great turbulent
+                clouds a very small stage in a vast cosmic arena hydrogen atoms a very small stage
+                in a vast cosmic arena.
+            </p>
+        </div>
+    )
+}
+
 function Sidebar() {
     return (
-        <aside className="Sidebar">Sidebar</aside>
+        <aside className="Sidebar">
+            <button className="ButtonNew"> + New Note </button>
+            <Note></Note>
+        </aside>
     );
 }
 
